352f1adc32597ce0d31c1e6a9c42fa19343f280a
Thomas Schwappach <Thomas.schwappach@gmail.com>
2015-11-03 14:27:03 -0700

    initial commit

---
+++ Test Questions/src/BankAccount.java
@@ -0,0 +1,30 @@
+
+public class BankAccount
+	{
+			private double myBalance;
+				
+			 public BankAccount ()
+				 { 
+					 myBalance = 0;
+				 }
+				
+			 public BankAccount(double balance)
+				 {
+				 	myBalance = balance;
+				 }
+				
+			 public void deposit(double amount)
+				 { 
+					 myBalance += amount;
+				 }
+				
+			 public void withdraw(double amount)
+				 {
+					 myBalance -= amount;
+				 }
+				
+			 public double getBalance( )
+				 { 
+				 	return myBalance;
+				 }
+	 }
---
+++ Test Questions/src/Runner.java
@@ -0,0 +1,22 @@
+
+public class Runner
+	{
+
+		public static void main(String[] args)
+			{
+//				BankAccount b=new BankAccount();
+//				BankAccount b=new SavingsAccount();
+//				BankAccount b=new CheckingAccount();
+//				SavingsAccount s=new BankAccount();
+//				SavingsAccount s=new SavingsAccount();
+//				SavingsAccount s=new CheckingAccount();
+//				CheckingAccount c=new BankAccount();
+//				CheckingAccount c=new SavingsAccount();
+//				CheckingAccount c= new CheckingAccount();
+				
+				
+				
+
+			}
+
+	}
